32ddeb5848d18abe43f5847e3e69a40c076adb5d
danielphilip12 <daniel.gallo12@gmail.com>
2025-10-06 15:33:55 -0700

    Add modulo calculation

---
+++ modulo.py
@@ -0,0 +1,11 @@
+def modulo(x, y):
+    """
+    Created by: Daniel 
+    Inputs: 
+        x: first number to be divided
+        y: second number to be divided by
+    Outputs:
+        result: the remainder of the division of x / y
+    """
+    result = x % y
+    return result
