021916e4ed50b3e22872634267cbb9bcb43cee98
danielphilip12 <daniel.gallo12@gmail.com>
2025-10-06 15:36:19 -0700

    Add exponent calculation

---
+++ exponent.py
@@ -0,0 +1,12 @@
+def exp(x, y):
+    """
+    Created by: Daniel
+    Inputs:
+        x: the base number
+        y: the power the base number should go to
+    outputs:
+        result: the result of x to the power of y
+    """
+    result = x ** y
+    return result
+
